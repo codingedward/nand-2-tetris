777740525c2e7efef152312b841dac03663670d2
Edward Njoroge <codingedward@gmail.com>
2019-04-30 22:10:11 +0300

    Add mult.asm implementation

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,23 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Assume x = *R0, y = *R1, product = *R2
+
+    @R2
+    M=0         // product = 0
+(LOOP)
+    @R0
+    M=M-1
+    D=M
+    @END      
+    D;JLT       // goto END if x <= 0
+    @R1
+    D=M         // D = y
+    @R2
+    M=M+D       // product += y
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
