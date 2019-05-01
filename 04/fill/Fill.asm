c96169fbfc00ed486efb391803be8354c18d8ce6
Edward Njoroge <codingedward@gmail.com>
2019-05-01 20:46:42 +0300

    Add screen fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,46 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(CHECK_KEY)
+    @i
+    M=0
+    @KBD
+    D=M
+    @BLACKEN
+    D;JGT       // jump to blacken if kbd > 0
+
+(WHITEN)
+    @i
+    D=M
+    @8192
+    D=D-A
+    @CHECK_KEY
+    D;JEQ       // (i-8192) == 0 done whitening!
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+    @i
+    M=M+1
+    @WHITEN
+    0;JMP
+
+(BLACKEN)
+    @i
+    D=M
+    @8192
+    D=D-A
+    @CHECK_KEY
+    D;JEQ       // (i-8192) == 0 done blackening
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1
+    @i
+    M=M+1
+    @BLACKEN
+    0;JMP
+
+
